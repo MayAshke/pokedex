082a72ba1155a19fd063d17057c76c0cba65d281
May Ashkenazi <mayas@moveo.co.il>
2025-05-12 10:30:28 +0300

    open project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Component, OnInit } from '@angular/core';
+import { PokemonService } from '../services/pokemon.service';
+import { CommonModule } from '@angular/common';
+import { PokemonCardComponent } from '../components/pokemon-card/pokemon-card.component';
+import { PokemonGridComponent } from '../components/pokemon-grid/pokemon-grid.component';
+import { SearchBarComponent } from '../components/search-bar/search-bar.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [CommonModule, PokemonCardComponent, PokemonGridComponent, SearchBarComponent],
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'pokedex';
+  pokemons: any[] = [];
+
+  constructor(private pokemonService: PokemonService) {}
+
+  ngOnInit() {
+    this.pokemonService.getPokemonList().subscribe(response => {
+      this.pokemons = response.results;
+    });
+  }
+
+  onSearchPokemon(term: string) {
+    console.log('חיפוש על:', term);
+  }
 }
